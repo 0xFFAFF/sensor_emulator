730ff1839b9c13d991ea47c7c1293450ab2804cb
ta <74548040+0xFFAFF@users.noreply.github.com>
2022-05-06 11:02:54 +0800

    v3.0

--- src/code/Server.py
+++ src/code/Server.py
@@ -22,6 +22,8 @@ conn_pool = [] #socket连接池
 
 sendqueue = Queue()
 
+recv_command={'client':None,'cmd':'realtime'}
+
 '''
 receive_thread()为服务器的发送函数，为了解决input输入导致的线程阻塞，无法接受数据问题。
 
@@ -31,13 +33,18 @@ args:
     recvQueue: 接收队列，把接受到的数据放到这个队列中
 '''
 def receive_thread(clientSocket,clientAddr,database):
-    collect = database['{}:{}'.format(clientAddr[0],clientAddr[1])]
+    temperature_collect = database['temperature']
+    humidity_collect = database['humidity']
     
     while True:
         try:
             data = json.loads(clientSocket.recv(BUFFSIZE).decode('utf-8'))
-            collect.insert_one(data)
-            # print('clientAddr:{clientAddr},Data:{data}'.format(clientAddr=clientAddr,data=data))
+            if(data['data_type'] == 'temprature'):
+                temperature_collect.insert_one(data)
+            if(data['data_type'] == 'humidity'):
+                humidity_collect.insert_one(data)
+            if(recv_command['cmd'] == 'realtime' and recv_command['client'] == clientAddr):
+                print('clientAddr:{clientAddr},Data:{data}'.format(clientAddr=clientAddr,data=data))
         except ConnectionResetError:
             print('Client {} 关闭了连接,接受线程已退出'.format(clientAddr))
             clientSocket.close()
@@ -56,6 +63,8 @@ class Server():
         self.serverSocket.listen(10)
         self.client = pymongo.MongoClient(host='localhost', port=27017) 
         self.db = self.client['sensor_data']
+        temperature_data = self.db['temperature_data']
+        humidity_data = self.db['humidity_data']
         
     def accept(self):
         while True:
@@ -83,37 +92,65 @@ class Server():
 
     def ui(self):
         while True:
+            try:
                 info = input('请输入要发送的信息：\n\
                                 1.stop --传感器停止发送数据\n\
                                 2.start --传感器开始发送数据\n\
                                 3.interval {n} --设置传感器的发送间隔为n秒\n\
-                                4.real time --获取实时数据\n');
-                if (info != 'stop') & (info != 'start') & (re.match('interval [0-9]+',info)==None):
-                    print('信息格式有误，请重新输入\n')
-                    continue
-                print('当前在线客户端为：')
-                if(len(conn_pool)==0):
-                    print('当前没有客户端在线\n')
-                    continue
-                for index,clientSocket in enumerate(iterable=conn_pool):
-                    print('ID:{},地址为：{}'.format(index,clientSocket.getpeername()))
-                    
-                choose_client = input('输入信息要送往的客户端ID\n')
-                for index,clientSocket in enumerate(iterable=conn_pool):
-                    if index == int(choose_client):
-                        try:
-                            clientSocket.send(info.encode('utf-8'))
-                            print('信息已经发送')
-                            print('目的地址：{}'.format(clientSocket.getpeername()))
-                            break
-                        except ConnectionResetError:
-                            print('Client {} 关闭了连接,发送线程已退出'.format(clientAddr))
-                            break
-                        except TimeoutError:
-                            print('发送超时')
-                            break
+                                4.realtime --获取实时数据\n\
+                                5.query --查询数据\n');
+                if (info == 'stop') or (info == 'start') or (re.match('interval [0-9]+',info)!=None):
+                    print('当前在线客户端为：')
+                    if(len(conn_pool)==0):
+                        print('当前没有客户端在线\n')
+                        continue
+                    for index,clientSocket in enumerate(iterable=conn_pool):
+                        print('ID:{},地址为：{}'.format(index,clientSocket.getpeername()))
+                        
+                    choose_client = input('输入信息要送往的客户端ID\n')
+                    for index,clientSocket in enumerate(iterable=conn_pool):
+                        if index == int(choose_client):
+                            try:
+                                clientSocket.send(info.encode('utf-8'))
+                                print('信息已经发送')
+                                print('目的地址：{}'.format(clientSocket.getpeername()))
+                                break
+                            except ConnectionResetError:
+                                print('Client {} 关闭了连接,发送线程已退出'.format(clientAddr))
+                                break
+                            except TimeoutError:
+                                print('发送超时')
+                                break
                 
-        
+                if info == 'realtime':
+                    print('当前在线客户端为：')
+                    if(len(conn_pool)==0):
+                        print('当前没有客户端在线\n')
+                        continue
+                    for index,clientSocket in enumerate(iterable=conn_pool):
+                        print('ID:{},地址为：{}'.format(index,clientSocket.getpeername()))
+                        
+                    choose_client = input('输入信息要送往的客户端ID\n')
+                    recv_command['client'] = conn_pool[int(choose_client)].getpeername()
+                    recv_command['cmd'] = 'realtime'
+                    
+                if info == 'query':
+                    list = self.db.list_collection_names()
+                    print(list)
+                    choose_client = input('输入信息要送往的客户端ID\n')
+                    print(list[int(choose_client)])
+                    collect = self.db[list[int(choose_client)]]
+                    data = collect.find()
+                    for i in data:
+                        print(i)
+                    
+                else:
+                    print('输入错误，请重新输入')
+
+            except KeyboardInterrupt:
+                recv_command['client'] = None
+            except EOFError:
+                recv_command['client'] = None
         
     def run(self):
         uiThread = threading.Thread(target=self.ui)
